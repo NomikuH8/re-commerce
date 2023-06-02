44f35a78931d55bd516acd566d4ab5ffbfb77f4b
joao menna <joao.menna@innovo.com.br>
2023-06-02 08:53:27 -0300

    adicionado link para repositorio no about

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,13 +5,24 @@ export default function AboutPage() {
     <AboutArticle>
       <p>Re:commerce é um projeto acadêmico de brincadeira.</p>
       <p>
-        O grupo que fez esse site condena as práticas reais de abuso,
-        compra e venda de animais silvestres.
+        O grupo que fez esse site condena as práticas reais de abuso, compra e venda de animais
+        silvestres.
+      </p>
+      <p>
+        Código fonte do site{" "}
+        <a
+          href='https://github.com/NomikuH8/re-commerce'
+          target='_blank'
+          referrerPolicy='no-referrer'
+        >
+          aqui
+        </a>
+        .
       </p>
     </AboutArticle>
   )
 }
 
 const AboutArticle = styled.article`
-padding: 1%;
+  padding: 1%;
 `
